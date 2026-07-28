7af6a2246f7fd78dd45985d001f32f096ddea115
ahzan-22 <ahmadzaky22122007@gmail.com>
2026-07-28 10:50:19 +0700

    membuat jawaban latihan-1

---
+++ bab-03-variable/latihan/latihan-1/index.js
@@ -0,0 +1,11 @@
+console.log("===== BIODATA =====");
+
+const namaLengkap = "Ahmad Zaky Nashrullah";
+let umur = 18;
+let kota = "Lubuklinggau";
+let pekerjaan = "Pelajar";
+
+console.log("namaLengkap :", namaLengkap);
+console.log("umur        :", umur, "tahun");
+console.log("kota        :", kota);
+console.log("pekerjaan   :", pekerjaan);
