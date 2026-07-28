3ee6fed09cede9f7662fb1213baf3d6955bd0d9c
ahzan-22 <ahmadzaky22122007@gmail.com>
2026-07-28 13:38:15 +0700

    membuat jawaban latihan-3

---
+++ bab-03-variable/latihan/latihan-3/index.js
@@ -0,0 +1,12 @@
+const namaProduk = "Chip Processor";
+const hargaSatuan = 3000000;
+const jumlahBeli = 1000;
+
+const totalHarga = hargaSatuan * jumlahBeli;
+
+console.log("--- Struk Belanja ---");
+console.log("Produk        : " + namaProduk);
+console.log("Harga Satuan  : " + hargaSatuan);
+console.log("Jumlah Beli   : " + jumlahBeli);
+console.log("----------------------");
+console.log("Total Belanja : " + totalHarga);
