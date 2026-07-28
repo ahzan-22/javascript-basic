9b5275cd51c15901feae4851c5cbbb2820d38f49
ahzan-22 <ahmadzaky22122007@gmail.com>
2026-07-28 13:50:25 +0700

    membuat jawaban latihan-4

---
+++ bab-03-variable/latihan/latihan-4/index.js
@@ -0,0 +1,8 @@
+let nama = "Ahmad Zaky Nashrullah";
+let tahunLahir = "2007";
+let tahunSekarang = "2026";
+
+let umur = tahunSekarang - tahunLahir;
+
+console.log("Assalamua'alaikum, nama saya " + nama + ".");
+console.log("Sekarang saya berumur " + umur + " tahun.");
