4b3b92f3a62cb943dffd94e2d972e07d39a34b67
ahzan-22 <ahmadzaky22122007@gmail.com>
2026-07-28 14:06:43 +0700

    membuat jawaban latihan-5

---
+++ bab-03-variable/latihan/latihan-5/index.js
@@ -0,0 +1,11 @@
+const nilaiMatematika = 87;
+const niaiIPA = 89;
+const nilaiBahasaIndonesia = 96;
+
+const rataRata = (nilaiMatematika + niaiIpa + nilaiBahasaIndonesia) / 3;
+
+console.log("Nilai Matematika       : " + nilaiMatematika);
+console.log("Nilai Ipa              : " + niaiIPA);
+console.log("Nilai Bahasa Indonesia : " + nilaiBahasaIndonesia);
+console.log("-----------------------------------");
+console.log("Rata Rata Nilai        : " + rataRata);
