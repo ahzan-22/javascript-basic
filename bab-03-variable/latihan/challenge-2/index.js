5262d6f84433ddd3d022f7dade88089761cf28a6
ahzan-22 <ahmadzaky22122007@gmail.com>
2026-07-28 14:48:12 +0700

    membuat jawaban challenge-2

---
+++ bab-03-variable/latihan/challenge-2/index.js
@@ -0,0 +1,17 @@
+const namaLengkap = "Ahmad Zaky Nashrullah";
+let umur = "18";
+let kota = "Lubuklinggau";
+const email = "ahmadzakynashrullah@gmail.com";
+const hobi = "Bersepeda & Main Game";
+const citaCita = "CEO";
+
+console.log("===========================");
+console.log("      PROFIL PENGGUNA      ");
+console.log("===========================");
+console.log("Nama Lengkap : " + namaLengkap);
+console.log("Umur         : " + umur);
+console.log("Kota         : " + kota);
+console.log("Email        : " + email);
+console.log("Hobi         : " + hobi);
+console.log("Cita-Cita    : " + citaCita);
+console.log("===========================");
