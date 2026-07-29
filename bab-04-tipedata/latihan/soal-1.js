28a37c628035946bcd88f4d39a3b2029a29a64de
ahzan-22 <ahmadzaky22122007@gmail.com>
2026-07-29 10:51:26 +0700

    mengerjakan soal-1

---
+++ bab-04-tipedata/latihan/soal-1.js
@@ -0,0 +1,11 @@
+const nama = "Ahmad Zaky Nashrullah"; //Ini String
+let umur = 18; //Ini Number
+let sudahLulus = true; //Ini Booelean
+let alamt = null; //Ini Null
+let nomorTelepon; //Ini Undefined
+
+console.log("Nama:", nama);
+console.log("Umur:", umur);
+console.log("Sudah Lulus:", sudahLulus);
+console.log("Alamat:", alamat);
+console.log("Nomor Telepon:", nomorTelepon);
